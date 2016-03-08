4bfd13306ec3e59c0db604911a1081793b780414
dbarclift <Douglasbarclift@gmail.com>
2016-03-08 02:02:53 +0000

    test two func master complete

--- projects/function-master/function-master.js
+++ projects/function-master/function-master.js
@@ -1,1 +1,13 @@
+//objectValues() : Should take an object and return its values in an array
+function objectValues(obj){
+    var out= [];
+    for(var key in obj){
+        out.push(obj[key]);
+    }
 
+    return out;
+}
+
+function keysToString(obj){
+    return Object.keys(obj).join(" ");
+}
