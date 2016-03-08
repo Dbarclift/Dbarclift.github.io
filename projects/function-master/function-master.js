d2bf3ba0bcb2417eca1ee28bcf98078b5a421f51
dbarclift <Douglasbarclift@gmail.com>
2016-03-08 03:07:53 +0000

    Dougs funcMaster up to question5

--- projects/function-master/function-master.js
+++ projects/function-master/function-master.js
@@ -11,3 +11,40 @@ function objectValues(obj){
 function keysToString(obj){
     return Object.keys(obj).join(" ");
 }
+
+function valuesToString(obj){
+    var out="";
+   for(var key in obj){
+        if (typeof obj[key]=== "string")
+            out+= obj[key]+ " ";
+    }
+    return out.trim();
+}
+
+
+function arrayOrObject(collection){
+  
+   if (Array.isArray(collection)) 
+   return "array";
+   
+  if(typeof collection === 'object')
+   return "object";
+}
+ function capitalizeWord (str){
+     var firstLetter = str[0];
+     var restOfWord = str.substring(1);
+     return firstLetter.toUpperCase()+ restOfWord;
+ }
+
+
+function capitalizeAllWords(str){
+    var split = str.split(" ");
+    var out = " ";
+    for( var i=0; i < split.length; i++){
+        out+= capitalizeWord(split[i])+ " ";
+    }
+    return str.trim();
+}
+   
+    
+    
