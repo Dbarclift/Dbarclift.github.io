e2f3218613bf9c2aed4ef083b9e554129d208a51
dbarclift <Douglasbarclift@gmail.com>
2016-03-09 02:32:36 +0000

    test eight  func master complete

--- projects/function-master/function-master.js
+++ projects/function-master/function-master.js
@@ -44,7 +44,35 @@ function capitalizeAllWords(str){
         out+= capitalizeWord(split[i])+ " ";
     }
     return str.trim();
+
+}   
+function capitalizeAllWords (str) {
+   var split = str.split(" ");
+   var out = "";
+   for (var i =0; i < split.length; i++) {
+       out += capitalizeWord(split[i]) + " ";
+    }
+   return out.trim();
 }
-   
+
+
+//Should take an object with a name property and return 'Welcome <Name>!'"
+function welcomeMessage(obj){
+ return "Welcome " + capitalizeWord(obj.name) + "!";
+}
+
+
+//Should take an object with a name an a species and return '<Name> is a <Species>'"
+function profileInfo(obj){
+       return capitalizeWord(obj.name) + " is a " + capitalizeWord(obj.species)
+}
+
+//Should take an object, if this object has a noises array return them as a string separated by a space, if there are no noises return 'there are no noises'"
+
+function maybeNoises(obj){
+    var noises=[];
+    for(var i=0; i< noises.length; i++)
+        return "there are no noises";
     
-    
+}
+
